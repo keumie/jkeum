6413d122cee655241a81596ac7a695566b9029c4
keumie <keum132@gmail.com>
2021-03-09 01:10:52 -0500

    small changes again

--- my-app/src/component/Project/Text.js
+++ my-app/src/component/Project/Text.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "20px",
+  },
+  text: {
+    fontSize: 14,
+    fontFamily:
+      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
+  },
+});
 
 const Test = () => {
+  const classes = useStyles();
   return (
-    <>
-      <p>hello</p>
-    </>
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Typography>Title</Typography>
+        <Typography>Tools</Typography>
+        <Typography>Description</Typography>
+        <Typography>Links</Typography>
+      </Grid>
+    </div>
   );
 };
 
