ba549fe212e1a3cd862423e8bfc1fcdc4e2259c0
keumie <keum132@gmail.com>
2021-03-11 13:18:08 -0500

    added working form

--- my-app/src/component/Contact/form.js
+++ my-app/src/component/Contact/form.js
@@ -1,3 +1,6 @@
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import { useForm } from "@formspree/react";
 
 const Form = () => {
@@ -9,11 +12,52 @@ const Form = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" name="email" />
+      <Grid container direction="column" spacing={3} xs={12}>
+        <Grid item>
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            defaultValue=""
+            type="email"
+            required
+          />
+        </Grid>
+        <Grid item>
+          <TextField id="name" name="name" label="Name" type="text" required />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="message"
+            name="message"
+            label="Message"
+            multiline
+            type="text"
+            required
+            rows={4}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+      {/* <label htmlFor="email">Email</label>
+      <input id="email" type="email" name="email" required="" />
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" name="name" required="" />
+      <label htmlFor="message">Message</label>
+      <textarea
+        rows="5"
+        name="message"
+        id="message"
+        placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus."
+        required=""
+      />
       <button type="submit" disabled={state.submitting}>
-        Sign up
-      </button>
+        Send Email
+      </button> */}
     </form>
   );
 };
--- my-app/src/component/Contact/index.js
+++ my-app/src/component/Contact/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { Container, Header, Underline } from "../CommonElement";
+import Form from "./form";
 
 const Contact = () => {
   return (
@@ -9,6 +10,7 @@ const Contact = () => {
         <Header>contact</Header>
         <Underline />
       </Grid>
+      <Form />
     </Container>
   );
 };
