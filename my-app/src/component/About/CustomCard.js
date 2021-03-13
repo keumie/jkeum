44eea4a04de21180756093f1588ee5469c16015c
keumie <keum132@gmail.com>
2021-03-12 20:06:57 -0500

    clean up on title and about me page

--- my-app/src/component/About/CustomCard.js
+++ my-app/src/component/About/CustomCard.js
@@ -1,47 +1,39 @@
 import React from "react";
-import cx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import TextInfoContent from "./TextInfoContent";
 
-const useStyles = makeStyles(({ breakpoints, spacing }) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     margin: "auto",
-    borderRadius: spacing(2), // 16px
-    transition: "0.3s",
-    boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
-    position: "relative",
-    marginLeft: "auto",
+    borderRadius: "16px",
+    boxShadow: "5px 5px 50px 5px rgba(0,0,0)",
     overflow: "initial",
-    background: "#ffffff",
     display: "flex",
-    flexDirection: "column",
-    paddingBottom: spacing(2),
     width: "70%",
-    [breakpoints.up("md")]: {
-      flexDirection: "row",
-      paddingTop: spacing(2),
+    flexDirection: "row",
+    padding: "16px 0",
+    [theme.breakpoints.down(700)]: {
+      padding: "0",
+      flexDirection: "column",
     },
   },
   media: {
-    // width: "200px",
-    // height: "200px",
-    // marginLeft: "auto",
-    // marginTop: spacing(-3),
-    // paddingBottom: "48%",
     flexShrink: 0,
-    borderRadius: spacing(2),
-    backgroundColor: "#fff",
+    borderRadius: "16px",
+    backgroundColor: "white",
     position: "relative",
-    // [breakpoints.up("md")]: {
     width: "250px",
     height: "450px",
-    marginRight: spacing(-3),
-    marginTop: 0,
+    marginRight: "-24px",
     transform: "translateX(8px)",
-    // },
+    [theme.breakpoints.down(700)]: {
+      transform: "translateX(0px)",
+      margin: "0",
+      width: "auto",
+    },
     "&:after": {
       content: '" "',
       position: "absolute",
@@ -50,42 +42,27 @@ const useStyles = makeStyles(({ breakpoints, spacing }) => ({
       width: "100%",
       height: "100%",
       backgroundImage: "linear-gradient(147deg, #fe8a39 0%, #757575 75%)",
-      borderRadius: spacing(2), // 16
+      borderRadius: "16px",
       opacity: 0.2,
     },
   },
-  boxStyle: {
-    fontFamily:
-      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
+  text: {
     width: "auto",
   },
 }));
 
-const shadowStyle = makeStyles({
-  root: ({ inactive }) => ({
-    boxShadow: "5px 5px 50px 5px rgba(0,0,0)",
-    // transition: "0.3s",
-    // ...(!inactive && {
-    //   "&:hover": {
-    //     transform: "translateY(-3px)",
-    //     boxShadow: "0 4px 20px 0 rgba(0,0,0,0.12)",
-    //   },
-    // }),
-  }),
-});
-
-export const CustomCard = React.memo(function BlogCard(props) {
+export const CustomCard = (props) => {
   const { logo } = props;
-  const styles = useStyles();
-  const shadowStyles = shadowStyle();
+  const classes = useStyles();
+
   return (
-    <Card className={cx(styles.root, shadowStyles.root)}>
-      <CardContent className={styles.boxStyle}>
+    <Card className={classes.root}>
+      <CardContent className={classes.text}>
         <TextInfoContent />
       </CardContent>
-      <CardMedia className={styles.media} image={logo} />
+      <CardMedia className={classes.media} image={logo} />
     </Card>
   );
-});
+};
 
 export default CustomCard;
